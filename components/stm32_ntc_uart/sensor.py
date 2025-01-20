2880621974e939b7d8d2c7e0d8026d5923c2dc02
Tom Krüsi <t.kruesi@bitlane.ch>
2025-01-20 17:06:13 +0100

    using separate sensor.py

--- components/stm32_ntc_uart/sensor.py
+++ components/stm32_ntc_uart/sensor.py
@@ -9,44 +9,49 @@ from esphome.const import (
     STATE_CLASS_MEASUREMENT,
 )
 
-CODEOWNERS = ["@Bitlanch"]  # optional
-DEPENDENCIES = ["uart"]           # Wir brauchen den UART
+# Sagt ESPHome, dass dieses Verzeichnis eine Sensor-Plattform enthält
+PLATFORMS = ["sensor"]
 
-# Erzeuge einen Namespace "stm32_ntc_uart" in esphome
+# Wir brauchen "uart" als Abhängigkeit
+DEPENDENCIES = ["uart"]
+
+# (Optional) wer betreut diesen Code
+CODEOWNERS = ["@DeinGitHubUser"]
+
+# Namespace anlegen
 stm32_ntc_uart_ns = cg.esphome_ns.namespace("stm32_ntc_uart")
 
-# Die C++-Klasse, wie sie im Header definiert ist:
+# Aus stm32_ntc_uart_ns die Klasse STM32NTCUART "importieren"
+# (Name + Basisklassen müssen zum C++-Code passen!)
 STM32NTCUART = stm32_ntc_uart_ns.class_(
-    "STM32NTCUART",   # Klassenname in C++
-    cg.Component,     # erbt von esphome::Component
+    "STM32NTCUART",   # C++-Klassenname
+    cg.Component,     # erbt in C++ von esphome::Component
     uart.UARTDevice,  # erbt von esphome::uart::UARTDevice
     sensor.Sensor     # erbt von esphome::sensor::Sensor
 )
 
-# YAML-Konfiguration: wir bauen eine "Sensor-Plattform"
-# - Standard-Felder wie Name, Unit, Icon, ...
-# - plus das UART-Device-Schema
+# CONFIG_SCHEMA:
+# Da du "sensor::Sensor" beerbst, nutzen wir sensor.sensor_schema(...)
 CONFIG_SCHEMA = sensor.sensor_schema(
     STM32NTCUART,
     unit_of_measurement=UNIT_CELSIUS,
     icon=ICON_THERMOMETER,
     accuracy_decimals=1,
     device_class=DEVICE_CLASS_TEMPERATURE,
-    state_class=STATE_CLASS_MEASUREMENT
+    state_class=STATE_CLASS_MEASUREMENT,
 ).extend({
     cv.GenerateID(): cv.declare_id(STM32NTCUART),
 }).extend(uart.UART_DEVICE_SCHEMA)
 
-# to_code() wird aufgerufen, wenn ESPHome die C++-Instanzen erzeugt
+
 def to_code(config):
-    # Hole das Objekt von "uart_id: ..." aus der YAML
-    # (Dieser Key kommt aus `uart.UART_DEVICE_SCHEMA`)
+    """Wird von ESPHome aufgerufen, um das C++-Objekt anzulegen."""
+    # Holt das UART-Objekt (z. B. id: my_uart_bus) aus config
     parent = yield cg.get_variable(config[uart.CONF_UART_ID])
-    # Neues C++-Objekt anlegen
+    # Erstellt ein C++-Objekt vom Typ STM32NTCUART
     var = cg.new_Pvariable(config[CONF_ID], parent)
-    # Komponente registrieren (damit setup(), loop() etc. laufen)
+
+    # Registrierung bei ESPHome:
     cg.register_component(var, config)
-    # UART registrieren
     cg.register_uart_device(var, config)
-    # Sensor registrieren (damit publish_state() etc. weiß, wie er heißen soll)
     sensor.register_sensor(var, config)
