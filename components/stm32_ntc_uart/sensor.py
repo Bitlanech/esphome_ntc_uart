830dfb3f5c6eb9e33c539a46e638f11c8a66a3aa
Tom Krüsi <t.kruesi@bitlane.ch>
2025-01-20 17:26:16 +0100

    leaving yield

--- components/stm32_ntc_uart/sensor.py
+++ components/stm32_ntc_uart/sensor.py
@@ -9,38 +9,47 @@ from esphome.const import (
     UNIT_CELSIUS,
 )
 
+# Wir definieren eine Sensor-Plattform
 PLATFORMS = ["sensor"]
 DEPENDENCIES = ["uart"]
 
+# (Optional) wer sich kümmert
+CODEOWNERS = ["@DeinGitHubUser"]
+
+# Namespace passend zu deinem .h (z. B. namespace esphome::stm32_ntc_uart)
 stm32_ntc_uart_ns = cg.esphome_ns.namespace("stm32_ntc_uart")
+
+# Der Klassenname muss zum .h passen (z. B. class STM32NTCUARTSensor)
 STM32NTCUARTSensor = stm32_ntc_uart_ns.class_(
     "STM32NTCUARTSensor",
-    cg.Component,       # Erbt von esphome::Component
-    uart.UARTDevice,    # Erbt von esphome::uart::UARTDevice
-    sensor.Sensor       # Erbt von esphome::sensor::Sensor
+    cg.Component,       # erbt von esphome::Component
+    uart.UARTDevice,    # erbt von esphome::uart::UARTDevice
+    sensor.Sensor       # erbt von esphome::sensor::Sensor
 )
 
+# YAML-Konfiguration: definieren, was man angeben darf
 CONFIG_SCHEMA = sensor.sensor_schema(
     STM32NTCUARTSensor,
     unit_of_measurement=UNIT_CELSIUS,
     icon=ICON_THERMOMETER,
     accuracy_decimals=1,
     device_class=DEVICE_CLASS_TEMPERATURE,
-    state_class=STATE_CLASS_MEASUREMENT
+    state_class=STATE_CLASS_MEASUREMENT,
 ).extend({
     cv.GenerateID(): cv.declare_id(STM32NTCUARTSensor),
 }).extend(uart.UART_DEVICE_SCHEMA)
 
-
+# Asynchrone to_code-Funktion:
+# Verwende await, NICHT yield!
 async def to_code(config):
-    # 1) Erzeuge/„declariere” das C++-Objekt
+    # C++-Objekt anlegen
     var = cg.new_Pvariable(config[CONF_ID])
 
-    # 2) Registriere es als Komponente (setup() / loop())
-    yield cg.register_component(var, config)
+    # 1) Komponente registrieren (setup/loop)
+    await cg.register_component(var, config)
 
-    # 3) Registriere es als UART-Device
-    yield uart.register_uart_device(var, config)
+    # 2) UART registrieren
+    await uart.register_uart_device(var, config)
 
-    # 4) Registriere es als Sensor (publish_state, Name etc.)
-    yield sensor.register_sensor(var, config)
+    # 3) Sensor registrieren (publish_state, name etc.)
+    await sensor.register_sensor(var, config)
