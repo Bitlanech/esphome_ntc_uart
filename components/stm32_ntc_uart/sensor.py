e91f853405d117b9dbcc983c677d000d6117c861
Tom Krüsi <t.kruesi@bitlane.ch>
2025-01-21 20:20:07 +0100

    this is getting me mad...

--- components/stm32_ntc_uart/sensor.py
+++ components/stm32_ntc_uart/sensor.py
@@ -6,55 +6,49 @@ from esphome.const import CONF_ID
 PLATFORMS = ["sensor"]
 DEPENDENCIES = ["uart"]
 
-# Namespace
 stm32_ntc_uart_ns = cg.esphome_ns.namespace("stm32_ntc_uart")
 
-# Deklariere die Sub-Sensor-Klasse, die in C++ MySubSensor heißt
-# und erbt von sensor::Sensor
 MySubSensor = stm32_ntc_uart_ns.class_(
-    "MySubSensor",
+    "MySubSensor",  # Muss C++-Klassennamen entsprechen
     sensor.Sensor
 )
-# Sag ESPHome: "Es gibt einen parameterlosen Konstruktor MySubSensor()"
-MySubSensor.add_constructor([], return_type=MySubSensor)
+# Parameterloser Konstruktor registrieren
+# => KEIN return_type=... verwenden!
+MySubSensor.add_constructor()
 
-# Hauptklasse
 STM32NTCUARTMulti = stm32_ntc_uart_ns.class_(
     "STM32NTCUARTMulti",
     cg.Component,
     uart.UARTDevice
 )
-
-# Meldung, dass es in STM32NTCUARTMulti eine Methode add_sensor(sensor::Sensor*) gibt
 STM32NTCUARTMulti.add_method(
     "add_sensor",
     cg.void,
-    [sensor.Sensor.operator("ptr")]
+    [sensor.Sensor.operator("ptr")]  # sensor::Sensor*
 )
 
-# Schema: mehrere Sub-Sensoren
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(STM32NTCUARTMulti),
     cv.Required("sensors"): cv.ensure_list(sensor.SENSOR_SCHEMA),
 }).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
-    # 1) Hauptobjekt
+    # Hauptobjekt anlegen
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    # 2) Schleife über die Sub-Sensoren
+    # Liste "sensors:" durchgehen
     for i, sconf in enumerate(config["sensors"]):
-        # Falls kein id: in YAML, legen wir ein Auto-ID an
+        # Falls kein id: angegeben, generieren wir einen
         if CONF_ID not in sconf:
             sconf[CONF_ID] = cg.declare_id(MySubSensor)(f"auto_sub_{i}")
 
         sub_id = sconf[CONF_ID]
 
-        # 3) Erzeuge ein Objekt: MySubSensor()
+        # Ein Objekt vom Typ MySubSensor() anlegen
         sub_sensor = cg.new_Pvariable(sub_id, MySubSensor)
-        # Parameter aus sconf anwenden (name, unit_of_measurement etc.)
         await sensor.register_sensor(sub_sensor, sconf)
-        # 4) In C++ add_sensor(sub_sensor) aufrufen
+
+        # In C++ => var->add_sensor(sub_sensor);
         cg.add(var.add_sensor(sub_sensor))
--- components/stm32_ntc_uart/stm32_ntc_uart.h
+++ components/stm32_ntc_uart/stm32_ntc_uart.h
@@ -1,44 +1,42 @@
 #pragma once
 
 #include <vector>
-#include <cstdlib>
 #include "esphome/core/component.h"
-#include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/uart/uart.h"
 
 namespace esphome {
 namespace stm32_ntc_uart {
 
-// 1) Sub-Sensor-Klasse (erbt von sensor::Sensor)
+// 1) Subsensor-Klasse, erbt von sensor::Sensor
 class MySubSensor : public sensor::Sensor {
  public:
-  // Expliziter, parameterloser Konstruktor (Verhinderung von Codegen-Bugs)
+  // Expliziter, parameterloser Konstruktor (ermöglicht new MySubSensor())
   MySubSensor() {
-    // Optional: Logging oder Initialisierung
-    // ESP_LOGD("mysubsensor", "MySubSensor() Konstruktor aufgerufen");
+    // optional: ESP_LOGD("mysubsensor", "Konstruktor aufgerufen");
   }
 };
 
-// 2) Die Multi-Klasse, die Sub-Sensoren verwaltet
+// 2) Hauptklasse
 class STM32NTCUARTMulti : public Component, public uart::UARTDevice {
  public:
-  // Diesen Funktionskopf muss Python kennen:  void add_sensor(sensor::Sensor *s)
+  // Im Python-Code rufen wir var.add_sensor(sub_sensor)
   void add_sensor(sensor::Sensor *s) {
     this->sensors_.push_back(s);
   }
 
   void setup() override {
-    ESP_LOGI("stm32_ntc_uart", "Setup done: %d Sub-Sensor(en)", (int)sensors_.size());
+    ESP_LOGI("stm32_ntc_uart", "Setup done, %d Sub-Sensor(en).",
+             (int) this->sensors_.size());
   }
 
   void loop() override {
-    // Beispiel: bis \n einlesen
     while (this->available()) {
       char c = this->read();
       if (c == '\n') {
         // parse float
         float val = std::strtof(buffer_.c_str(), nullptr);
-        // an alle Sub-Sensoren ausgeben
+        // Alle Sub-Sensoren erhalten diesen Wert
         for (auto *sens : sensors_) {
           sens->publish_state(val);
         }
