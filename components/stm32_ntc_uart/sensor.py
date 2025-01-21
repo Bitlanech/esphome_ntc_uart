771045a54f0122075dd6330fb0ffb46aa8705fa8
Tom Krüsi <t.kruesi@bitlane.ch>
2025-01-21 19:49:40 +0100

    try out withouth "hack"

--- components/stm32_ntc_uart/sensor.py
+++ components/stm32_ntc_uart/sensor.py
@@ -1,54 +1,44 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
-from esphome.const import (
-    CONF_ID,
-)
+from esphome.const import CONF_ID
 
-# Deklariere: "Dieses Verzeichnis stellt eine Sensor-Plattform bereit"
+# Unsere neue Sensor-Plattform
 PLATFORMS = ["sensor"]
-
-# Wir brauchen UART, da wir 'uart::UARTDevice' nutzen
 DEPENDENCIES = ["uart"]
 CODEOWNERS = ["@Bitlanech"]
 
-# Namespace (muss zu deinem C++-Namespace passen)
 stm32_ntc_uart_ns = cg.esphome_ns.namespace("stm32_ntc_uart")
 
-# Hier definieren wir eine Klasse, die NUR Component+UARTDevice erbt
-# (keine direkte sensor::Sensor-Vererbung)
+# Die C++-Klasse erbt von Component+UARTDevice
 STM32NTCUARTMulti = stm32_ntc_uart_ns.class_(
     "STM32NTCUARTMulti",
     cg.Component,
     uart.UARTDevice
 )
 
-# Eine "add_sensor"-Methode, um Sub-Sensoren an die C++-Klasse zu hängen
-# (die Signatur in C++ wird "void add_sensor(sensor::Sensor *);")
-AddSensorMethod = STM32NTCUARTMulti.operator("add_sensor").bind(sensor.Sensor)
+# Diese Zeile sagt ESPHome: 
+# "Es gibt eine Methode add_sensor(...) in STM32NTCUARTMulti mit Signatur void(sensor::Sensor *)"
+AddSensorMethod = STM32NTCUARTMulti.add_method(
+    "add_sensor",
+    cg.void,
+    [cg.pointer_to(sensor.Sensor)],
+)
 
-# Schema: Wir erwarten, dass der User eine Liste "sensors:" angeben kann
-# Jeder Eintrag in "sensors" muss ein valides sensor.SENSOR_SCHEMA sein
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(STM32NTCUARTMulti),
     cv.Required("sensors"): cv.ensure_list(sensor.SENSOR_SCHEMA),
 }).extend(uart.UART_DEVICE_SCHEMA)
 
-# Asynchrone to_code-Funktion
 async def to_code(config):
-    # 1) Erzeugung der C++-Hauptinstanz
+    # Haupt-C++-Objekt
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    # 2) Für jeden Eintrag in "sensors" erzeugen wir ein Sensor-Objekt
+    # Für jeden Eintrag in "sensors" legen wir einen Sensor an
     for i, sconf in enumerate(config["sensors"]):
-        # Erstellt ein sensor::Sensor-Objekt
         sub_sensor = await sensor.new_sensor_item(sconf)
-
-        # "Anmelden" bei ESPHome (damit publish_state, name etc. greifen)
-        # WICHTIG: Auf neueren ESPHome-Versionen async + await
         await sensor.register_sensor(sub_sensor, sconf)
-
-        # 3) An unsere C++-Klasse anhängen
+        # C++-Methode aufrufen
         cg.add(var.add_sensor(sub_sensor))
--- components/stm32_ntc_uart/stm32_ntc_uart.h
+++ components/stm32_ntc_uart/stm32_ntc_uart.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <vector>
-#include <cstdlib>  // strtof
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
@@ -12,22 +11,20 @@ namespace stm32_ntc_uart {
 class STM32NTCUARTMulti : public Component,
                           public uart::UARTDevice {
  public:
-  /// Aus Python-Seite: var.add_sensor(...)
+  // Muss EXACT diese Signatur haben:
+  //   void add_sensor(sensor::Sensor *s)
   void add_sensor(sensor::Sensor *s) {
     this->sensors_.push_back(s);
   }
 
   void setup() override {
-    ESP_LOGI("stm32_ntc_uart", "Setup done. Anzahl Sensoren: %d",
-             (int) this->sensors_.size());
+    ESP_LOGI("stm32_ntc_uart", "Setup done: %d Sensoren", (int)this->sensors_.size());
   }
 
   void loop() override {
-    // UART-Daten sammeln, bis zum Newline
     while (this->available()) {
       char c = this->read();
       if (c == '\n') {
-        // Zeile fertig -> verarbeiten
         process_line_(this->read_buffer_);
         this->read_buffer_.clear();
       } else {
@@ -38,78 +35,14 @@ class STM32NTCUARTMulti : public Component,
 
  protected:
   std::string read_buffer_;
-  std::vector<sensor::Sensor *> sensors_;
+  std::vector<sensor::Sensor*> sensors_;
 
-  /// Diese Funktion zerlegt den Zeileninhalt an Semikolon
-  /// und verteilt die Werte auf die Sensoren.
   void process_line_(const std::string &line) {
-    if (line.empty()) {
-      ESP_LOGW("stm32_ntc_uart", "Leere Zeile empfangen, ignorieren.");
-      return;
+    // Beispiel: 1 Wert für alle Sensoren
+    float val = std::strtof(line.c_str(), nullptr);
+    for (auto *s : sensors_) {
+      s->publish_state(val);
     }
-
-    // 1) Zerlege 'line' an jedem ';'
-    std::vector<std::string> tokens;
-    {
-      size_t start = 0;
-      while (true) {
-        size_t pos = line.find(';', start);
-        if (pos == std::string::npos) {
-          // letztes Token
-          tokens.push_back(line.substr(start));
-          break;
-        } else {
-          tokens.push_back(line.substr(start, pos - start));
-          start = pos + 1;
-        }
-      }
-    }
-
-    // 2) Wir verarbeiten minimal(tokens.size(), sensors_.size())
-    //    d.h. wenn mehr Tokens als Sensoren vorhanden sind, wird der Rest ignoriert
-    size_t count = std::min(tokens.size(), sensors_.size());
-
-    for (size_t i = 0; i < count; i++) {
-      float value;
-      // parse_float_() wandelt den String in float um (ohne Exceptions)
-      if (this->parse_float_(tokens[i], value)) {
-        ESP_LOGD("stm32_ntc_uart", "Sensor #%d: %.2f (Token: '%s')",
-                 (int)i + 1, value, tokens[i].c_str());
-        // Wert an den i-ten Sub-Sensor
-        sensors_[i]->publish_state(value);
-      } else {
-        ESP_LOGW("stm32_ntc_uart",
-                 "Fehler beim Parsen von '%s' (Sensor %d)",
-                 tokens[i].c_str(), (int)i + 1);
-      }
-    }
-  }
-
-  /// Hilfsfunktion: Verwandelt einen String in float (strtof),
-  /// entfernt vorab unbrauchbare Zeichen.
-  bool parse_float_(const std::string &raw, float &out_val) {
-    // a) Unerwünschte Zeichen entfernen
-    std::string sanitized;
-    sanitized.reserve(raw.size());
-    for (char c : raw) {
-      if ((c >= '0' && c <= '9') || c == '.' || c == '+' || c == '-') {
-        sanitized.push_back(c);
-      }
-    }
-    if (sanitized.empty()) {
-      return false;
-    }
-
-    // b) strtof
-    char *endptr = nullptr;
-    float val = std::strtof(sanitized.c_str(), &endptr);
-    if (endptr == sanitized.c_str()) {
-      // Keine gültige Zahl gelesen
-      return false;
-    }
-
-    out_val = val;
-    return true;
   }
 };
 
