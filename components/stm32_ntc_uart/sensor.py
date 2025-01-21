16afeeabd14dd5888efa712a2ed199a3a55725ea
Tom Krüsi <t.kruesi@bitlane.ch>
2025-01-21 20:07:07 +0100

    rendering config "id" if available

--- components/stm32_ntc_uart/sensor.py
+++ components/stm32_ntc_uart/sensor.py
@@ -5,42 +5,55 @@ from esphome.const import CONF_ID
 
 PLATFORMS = ["sensor"]
 DEPENDENCIES = ["uart"]
+CODEOWNERS = ["@Bitlanech"]
 
+# Namespace, der zu deiner C++-Klasse passt
 stm32_ntc_uart_ns = cg.esphome_ns.namespace("stm32_ntc_uart")
 
-# Deine C++-Klasse: Erbt nur von Component+UARTDevice, keine direkte Sensor-Vererbung
+# Klasse, die NUR von Component + UARTDevice erbt (kein direkter sensor::Sensor!)
 STM32NTCUARTMulti = stm32_ntc_uart_ns.class_(
     "STM32NTCUARTMulti",
     cg.Component,
     uart.UARTDevice
 )
 
-# Angeben, dass in C++ eine Methode existiert:
-#    void add_sensor(sensor::Sensor *s)
+# Wir sagen ESPHome: "C++-Methode add_sensor(sensor::Sensor *s) existiert"
 AddSensorMethod = STM32NTCUARTMulti.add_method(
     "add_sensor",
-    cg.void,
-    [sensor.Sensor.operator("ptr")]  # "ptr" statt "*"
+    cg.void,                        # Rückgabewert: void
+    [sensor.Sensor.operator("ptr")] # Parameter: sensor::Sensor*
 )
+
+# Sub-Sensor-Schema: Alle üblichen sensor-Felder + OPTIONAL ein id-Feld
+SUB_SENSOR_SCHEMA = sensor.SENSOR_SCHEMA.extend({
+    cv.Optional(CONF_ID): cv.declare_id(sensor.Sensor),
+})
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(STM32NTCUARTMulti),
-    cv.Required("sensors"): cv.ensure_list(sensor.SENSOR_SCHEMA),
+    # Mehrere Sub-Sensoren möglich:
+    cv.Required("sensors"): cv.ensure_list(SUB_SENSOR_SCHEMA),
 }).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
-    # 1) Haupt-C++-Objekt anlegen
+    # 1) Hauptobjekt anlegen
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    # 2) Für jeden Eintrag in "sensors" ein Sub-Sensor erzeugen
-    for sconf in config["sensors"]:
-        # a) Roh-Objekt erstellen (ohne ID)
-        sub_sensor = cg.new_Pvariable(None, sensor.Sensor)
+    # 2) Für jeden Sensor in der YAML-Liste "sensors"
+    for i, sconf in enumerate(config["sensors"]):
+        # a) Falls kein "id:" in YAML, generieren wir einen
+        if CONF_ID not in sconf:
+            sconf[CONF_ID] = cg.declare_id(sensor.Sensor)(f"auto_sub_sensor_{i}")
 
-        # b) bei ESPHome "registrieren", damit Name, Filters etc. aus sconf angewendet werden
+        sub_id = sconf[CONF_ID]
+
+        # b) Ein sensor::Sensor-Objekt anlegen
+        sub_sensor = cg.new_Pvariable(sub_id, sensor.Sensor)
+
+        # c) Bei ESPHome registrieren => Name, Einheit, Filter etc. aus sconf
         await sensor.register_sensor(sub_sensor, sconf)
 
-        # c) in C++-Klasse einhängen
+        # d) In unserer C++-Klasse verknüpfen
         cg.add(var.add_sensor(sub_sensor))
--- components/stm32_ntc_uart/stm32_ntc_uart.h
+++ components/stm32_ntc_uart/stm32_ntc_uart.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <cstdlib>  // strtof
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
@@ -11,6 +12,7 @@ namespace stm32_ntc_uart {
 class STM32NTCUARTMulti : public Component,
                           public uart::UARTDevice {
  public:
+  // Muss genau so sein: void add_sensor(sensor::Sensor *s)
   void add_sensor(sensor::Sensor *s) {
     this->sensors_.push_back(s);
   }
@@ -21,23 +23,25 @@ class STM32NTCUARTMulti : public Component,
   }
 
   void loop() override {
+    // UART-Daten einsammeln
     while (this->available()) {
       char c = this->read();
       if (c == '\n') {
-        // z. B. parse float und an alle Sub-Sensoren senden
-        float val = std::strtof(this->read_buffer_.c_str(), nullptr);
-        for (auto *sens : this->sensors_) {
+        // Beispiel: parse float
+        float val = std::strtof(this->buffer_.c_str(), nullptr);
+        // Veröffentliche an alle Sensoren
+        for (auto *sens : sensors_) {
           sens->publish_state(val);
         }
-        this->read_buffer_.clear();
+        this->buffer_.clear();
       } else {
-        this->read_buffer_.push_back(c);
+        this->buffer_.push_back(c);
       }
     }
   }
 
  protected:
-  std::string read_buffer_;
+  std::string buffer_;
   std::vector<sensor::Sensor *> sensors_;
 };
 
